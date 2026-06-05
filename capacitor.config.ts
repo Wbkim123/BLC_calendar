d80bbfa8413f1f2caceb071b51daa51ed5191da7
Hayden <kimwb027@gmail.com>
2026-06-05 20:33:28 +0900

    Update app branding and launcher icon

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'dev.pages.blccalendar',
-  appName: 'BLC Calendar',
+  appName: 'Unofficial BLC Tracker',
   webDir: 'build'
 };
 
